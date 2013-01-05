71e1b3205c9e3d1bf87059d41fdfd26cbe204f83
cubicdaiya <cubicdaiya@gmail.com>
2013-01-05 21:26:53 +0900

    add example

---
+++ examples/pool.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#include <ngx_core.h>
+#include <ngx_palloc.h>
+#include <ngx_string.h>
+
+int main (int argc, char *argv[]) {
+    ngx_str_t s = ngx_string("bokko");
+    char *sp;
+    ngx_pool_t *pool;
+    size_t len;
+
+    pool = ngx_create_pool(4096, NULL);
+    len  = ngx_strlen(s.data);
+
+    sp = ngx_palloc(pool, len + 1);
+    if (sp == NULL) {
+        printf("alloc failed\n");
+        return NGX_ERROR;
+    }
+
+    ngx_cpystrn(sp, s.data, len + 1);
+
+    printf("sp            :%s\n",  sp);
+    printf("ngx_strlen(sp):%ld\n", ngx_strlen(sp));
+
+    ngx_destroy_pool(pool);
+
+    return 0;
+}
