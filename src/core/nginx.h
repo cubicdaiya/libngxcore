6c356cea0703110fd7df166940b02e2e4ed4b9b4
cubicdaiya <cubicdaiya@gmail.com>
2014-03-19 05:54:16 +0900

    bumped version of nginx to 1.4.7

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -9,8 +9,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      1004005
-#define NGINX_VERSION      "1.4.5"
+#define nginx_version      1004007
+#define NGINX_VERSION      "1.4.7"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #define NGINX_VAR          "NGINX"
